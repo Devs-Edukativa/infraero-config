108779369da846f299e2f1913e8d967e84072662
Rafael Dantas Boeira <rafaeldantasboeira@gmail.com>
2025-03-28 12:50:55 -0300

    ajustes

---
+++ pnae-js.js
@@ -0,0 +1,6 @@
+function handleConteudoCompleto() {
+  const container = document.querySelector(
+    '.container-conteudo-completo-pnae',
+  );
+  container.classList.toggle('show-conteudo-completo-pnae'); // Alterna a classe 'show'
+}
